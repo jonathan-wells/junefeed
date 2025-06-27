6d18fc3f1c83c6b8e364b0422e07c05a64d30b4a
Jonathan Wells <jonwells90@gmail.com>
2025-06-26 23:37:23 -0400

    Fix switching to highlighted entries and implement link-following functionality.

--- junefeed/app.py
+++ junefeed/app.py
@@ -3,7 +3,7 @@ from textual.widgets import Static
 from textual.screen import Screen
 from textual import events
 
-from junefeed.feed import EntryCollection, Feed
+from junefeed.feed import EntryCollection, Entry, Feed
 from junefeed.config import config
 
 
@@ -33,11 +33,6 @@ class EntryCollectionScreen(Screen):
             self.entries = EntryCollection.from_cached()
         else:
             self.entries = EntryCollection.from_feeds(config.feeds)
-        self.idx = 0
-
-    def compose(self) -> ComposeResult:
-        self.screen.styles.background = '#191724'
-        self.sub_title = 'entries'
         self.widgets = []
         for i, entry in enumerate(self.entries, 0):
             widget = Static(f'[#6e6a86]{i:>4}.[/]  [#31748f]{entry.feed:<12}[/] {entry.title}')
@@ -45,14 +40,18 @@ class EntryCollectionScreen(Screen):
             widget.styles.text_overflow = 'clip'
             widget.styles.color = '#908caa'
             self.widgets.append(widget)
-        self.widgets[0].styles.color = '#f6c177'
+        self.nwidgets = len(self.widgets)
+        self.idx = 0
+
+    def compose(self) -> ComposeResult:
+        self.screen.styles.background = '#191724'
         yield from self.widgets 
  
     def on_mount(self):
         self.screen.show_horizontal_scrollbar = True
         self.screen.styles.scrollbar_size_horizontal = 10
         self.screen.styles.scrollbar_size_vertical = 0
-        self.nwidgets = len(self.widgets)
+        self.highlight_current()
     
     def on_key(self, event: events.Key) -> None:
         if event.key == 'h':
@@ -63,6 +62,7 @@ class EntryCollectionScreen(Screen):
                 self.scroll_down()
             self.highlight_current()
         elif event.key == 'k':
+            print('k')
             self.idx -= 1
             if self.idx < self.nwidgets - 12:
                 self.scroll_up()
@@ -70,8 +70,9 @@ class EntryCollectionScreen(Screen):
         elif event.key == 'l':
             self.scroll_right()
  
-    def open_entry(self):
-        pass
+    def open_entry(self, other):
+        assert isinstance(other, SingleEntryScreen)
+        other.idx = self.idx
 
     def highlight_current(self):
         if self.idx < 0:
@@ -87,60 +88,66 @@ class EntryCollectionScreen(Screen):
         
 class SingleEntryScreen(Screen):
     
-    def __init__(self):
+    def __init__(self, entries: list[Entry] = [], idx: int = 0):
         super().__init__()
-        self.entries = list(EntryCollection.from_cached())
+        if entries == []: 
+            self.entries = EntryCollection.from_cached().entries
+        else:
+            self.entries = entries
+        self.widgets = [Static(str(entry)) for entry in self.entries]
+        self.idx = idx
 
     def compose(self) -> ComposeResult:
         self.screen.styles.background = '#191724'
-        self.widgets = [Static(str(entry)) for entry in self.entries]
-        self.idx = 0
         yield self.widgets[self.idx]
 
-    def on_mount(self):
-        pass 
 
 class Junefeed(App):
     
     BINDINGS = [
-        ('f', 'switch_mode("feeds")', 'FeedScreen'),
-        ('c', 'switch_mode("entry_collection")', 'EntryCollectionScreen'),
-        ('e', 'switch_mode("single_entry")', 'SingleEntryScreen')
+        ('c', 'push_screen("entry_collection")'),
+        ('f', 'push_screen("feeds")'),
     ]
-    MODES = {
-        'feeds': FeedScreen,
-        'entry_collection': EntryCollectionScreen,
-        'single_entry': SingleEntryScreen 
-    }
-
+    
     def __init__(self, from_cached=True):
+        super().__init__()
         if from_cached:
             self.entries = EntryCollection.from_cached()
         else:
             self.entries = EntryCollection.from_feeds(config.feeds)
         self.feeds = [Feed(url, name) for (name, url) in config.feeds.items()]
-        super().__init__()
     
     def refresh_feeds(self):
         # This won't work because it is not the same entries as displayed in the screen 
         self.entries = EntryCollection.from_feeds(config.feeds)
 
     def on_mount(self):
-        self.title = 'Junefeed'
-        self.switch_mode('entry_collection')
+        self.install_screen(EntryCollectionScreen(), 'entry_collection')
+        self.install_screen(FeedScreen(), 'feeds')
+        self.push_screen('entry_collection')
+
+    async def switch_to_entry(self):
+        entry_collection = self.screen
+        assert isinstance(entry_collection, EntryCollectionScreen), type(entry_collection)
+        single_entry = SingleEntryScreen(entry_collection.entries.entries, entry_collection.idx)
+        await self.push_screen(single_entry)
     
-    def switch_to_entry(self):
-        current_index = self.MODES['entry_collection'].idx
-        print(current_index)
-
-
-
-    def on_key(self, event: events.Key) -> None:
-        if event.key == 'q':
-            self.exit()
+    def open_entry_link(self):
+        entry = self.screen
+        assert isinstance(entry, SingleEntryScreen)
+        entry = entry.entries[entry.idx]
+        self.open_url(entry.link)
+            
+    async def on_key(self, event: events.Key) -> None:
+        if event.key == 'c' and isinstance(self.screen, SingleEntryScreen):
+            await self.pop_screen()
+        if event.key == 'o' and isinstance(self.screen, SingleEntryScreen):
+            self.open_entry_link()
+        elif event.key == 'o' and isinstance(self.screen, EntryCollectionScreen):
+            await self.switch_to_entry()
         elif event.key == 'r':
             self.refresh_feeds()
-            self.mount()
-        elif event.key == 'o':
-            self.switch_to_entry()
-
+        elif event.key == 'x':
+            raise ValueError(self.screen_stack)
+        elif event.key == 'q':
+            self.exit()
