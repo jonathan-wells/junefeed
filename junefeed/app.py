3a9294b4f7f73860bb009b689635cc626bf65592
Jonathan Wells <jonwells90@gmail.com>
2025-06-28 10:13:55 -0400

    Begin implementing read/unread functions

--- junefeed/app.py
+++ junefeed/app.py
@@ -7,7 +7,6 @@ from junefeed.feed import EntryCollection, Entry, Feed
 from junefeed.config import config
 
 
-
 class FeedScreen(Screen):
     
     def __init__(self):
@@ -33,15 +32,31 @@ class EntryCollectionScreen(Screen):
             self.entries = EntryCollection.from_cached()
         else:
             self.entries = EntryCollection.from_feeds(config.feeds)
-        self.widgets = []
-        for i, entry in enumerate(self.entries, 0):
-            widget = Static(f'[#6e6a86]{i:>4}.[/]  [#31748f]{entry.feed:<12}[/] {entry.title}')
-            widget.styles.text_wrap = 'nowrap'
-            widget.styles.text_overflow = 'clip'
-            widget.styles.color = '#908caa'
-            self.widgets.append(widget)
-        self.nwidgets = len(self.widgets)
+        self.read_entries = []
+        self.build_widgets()
         self.idx = 0
+    
+    @property
+    def nwidgets(self):
+        return len(self.widgets)
+
+    def _entry_to_widget(self, entry, idx):
+        pad = self._feedpad
+        widget = Static(f'[#6e6a86]{idx:>4}. {entry.feed:>{pad}}[/] {entry.title}')
+        widget.styles.text_wrap = 'nowrap'
+        widget.styles.text_overflow = 'clip'
+        if not entry.is_read:
+            widget.styles.color = '#908caa'
+        else:
+            widget.styles.color = '#6e6a86'
+        return widget
+    
+    def build_widgets(self):
+        self._feedpad = max(len(feed) for feed in config.feeds.keys())
+        self.widgets = []
+        for i, entry in enumerate(self.entries, 1):
+            widget = self._entry_to_widget(entry, i)
+            self.widgets.append(widget)
 
     def compose(self) -> ComposeResult:
         self.screen.styles.background = '#191724'
@@ -52,7 +67,7 @@ class EntryCollectionScreen(Screen):
         self.screen.styles.scrollbar_size_horizontal = 10
         self.screen.styles.scrollbar_size_vertical = 0
         self.highlight_current()
-    
+
     def on_key(self, event: events.Key) -> None:
         if event.key == 'h':
             self.scroll_left()
@@ -70,11 +85,9 @@ class EntryCollectionScreen(Screen):
         elif event.key == 'l':
             self.scroll_right()
  
-    def open_entry(self, other):
-        assert isinstance(other, SingleEntryScreen)
-        other.idx = self.idx
-
     def highlight_current(self):
+        if self.nwidgets == 0:
+            return
         if self.idx < 0:
             self.idx = 0
         elif self.idx >= self.nwidgets - 1:
@@ -85,6 +98,14 @@ class EntryCollectionScreen(Screen):
             self.widgets[self.idx+1].styles.color = '#908caa'
         self.widgets[self.idx].styles.color = '#f6c177'
 
+    def mark_read(self):
+        if self.nwidgets == 0:
+            return
+        entry = self.entries.entries.pop(self.idx)
+        entry.mark_read()
+        self.read_entries.append(entry)
+        self.widgets.pop(self.idx).remove()
+        self.highlight_current()
         
 class SingleEntryScreen(Screen):
     
@@ -109,22 +130,14 @@ class Junefeed(App):
         ('f', 'push_screen("feeds")'),
     ]
     
-    def __init__(self, from_cached=True):
-        super().__init__()
-        if from_cached:
-            self.entries = EntryCollection.from_cached()
-        else:
-            self.entries = EntryCollection.from_feeds(config.feeds)
-        self.feeds = [Feed(url, name) for (name, url) in config.feeds.items()]
-    
-    def refresh_feeds(self):
-        # This won't work because it is not the same entries as displayed in the screen 
-        self.entries = EntryCollection.from_feeds(config.feeds)
-
     def on_mount(self):
         self.install_screen(EntryCollectionScreen(), 'entry_collection')
         self.install_screen(FeedScreen(), 'feeds')
         self.push_screen('entry_collection')
+
+    async def refresh_feeds(self):
+        entry_collection = EntryCollectionScreen(from_cached=False)
+        await self.push_screen(entry_collection)
 
     async def switch_to_entry(self):
         entry_collection = self.screen
@@ -139,15 +152,24 @@ class Junefeed(App):
         self.open_url(entry.link)
             
     async def on_key(self, event: events.Key) -> None:
-        if event.key == 'c' and isinstance(self.screen, SingleEntryScreen):
-            await self.pop_screen()
-        if event.key == 'o' and isinstance(self.screen, SingleEntryScreen):
-            self.open_entry_link()
-        elif event.key == 'o' and isinstance(self.screen, EntryCollectionScreen):
-            await self.switch_to_entry()
-        elif event.key == 'r':
-            self.refresh_feeds()
-        elif event.key == 'x':
-            raise ValueError(self.screen_stack)
-        elif event.key == 'q':
-            self.exit()
+        # Universal keys:
+        if event.key == 'r':
+            await self.refresh_feeds()
+        if isinstance(self.screen, SingleEntryScreen):
+            if event.key == 'c':
+                await self.pop_screen()
+            elif event.key == 'o':
+                self.open_entry_link()
+            elif event.key == 'q':
+                await self.pop_screen()
+        elif isinstance(self.screen, EntryCollectionScreen):
+            if event.key == 'o':
+                await self.switch_to_entry()
+            elif event.key == 'm':
+                self.screen.mark_read()
+            elif event.key == 'q':
+                self.exit()
+        elif isinstance(self.screen, Feed):
+            if event.key == 'q':
+                self.exit()
+
